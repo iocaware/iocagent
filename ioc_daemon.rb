f7d3ded397a4604c8c82aa84a1076d37bdbcc12f
Matt Jezorek <mjezorek@gmail.com>
2013-08-25 09:09:13 -0700

    Added POSIX agent

---
+++ ioc_daemon.rb
@@ -0,0 +1,27 @@
+$LOAD_PATH.unshift File.dirname($0)
+require 'rubygems'
+require 'RubyIOC'
+require 'yaml'
+require 'daemons'
+require_relative 'scanner'
+CONFIG_FILE = 'config.yml'
+
+class IOCDaemon
+	def initialize
+		$config = YAML.load_file(CONFIG_FILE)
+		$scanner = Scanner.new($config)
+	end
+
+	def run
+		$scanner.log("IOCAgent started at: #{$config['delay']} " + Time.now.to_s)
+		$scanner.register
+		Daemons.daemonize
+		while true
+			$scanner.check
+			sleep($config['delay'].to_i.minutes)
+		end
+	end
+end
+
+iocagent = IOCDaemon.new
+iocagent.run()
